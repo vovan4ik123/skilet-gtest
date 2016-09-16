4bf42cd6ae066bd31e79bf4001d15bbd99667fea
vovan4ik111 <123vovarambler.ru@rambler.ru>
2016-09-16 19:12:29 +0300

    4

--- unit_test/ExecuteTests_1.cpp
+++ unit_test/ExecuteTests_1.cpp
@@ -1,10 +1,26 @@
 #include <gtest\gtest.h>
 #include <gmock\gmock.h>
 
+#include <cstdlib>
+#include <iostream>
+
 #include "ProgramInfo.h"
 
 TEST(InfoTest1, ProgramVersion)
 {
 
 	ASSERT_TRUE( std::string(ProgramInfo::Instance()->getVersion()) == "0.0.1" );
+}
+
+
+
+
+
+int main(int argc, char* argv[]) {
+	::testing::InitGoogleMock(&argc, argv);
+	::testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+
+	system("pause");
+	return 0;
 }
